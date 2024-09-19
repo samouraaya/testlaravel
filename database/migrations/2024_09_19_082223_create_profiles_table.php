e3fa49a329e7a22f769df04edf511405e8ffb136
Mohamed HADIJI <mohamed.hadiji@web-atrio.com>
2024-09-19 10:26:39 +0200

    Created Profile model and migration

---
+++ database/migrations/2024_09_19_082223_create_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->unsignedBigInteger('administrator_id');
+            $table->string('image')->nullable();
+            $table->enum('status', ['inactive', 'pending', 'active'])->default('pending');
+            $table->timestamps();
+        
+            $table->foreign('administrator_id')->references('id')->on('administrators')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
