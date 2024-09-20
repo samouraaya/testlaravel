67d23d7329aa5bd3d98c09a383310756e2e23bab
Mohamed HADIJI <mohamed.hadiji@web-atrio.com>
2024-09-20 17:34:26 +0200

    Added Swagger documentation with L5-Swagger package

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,43 @@ use App\Models\Role;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-
-
+use OpenApi\Annotations as OA;
+   /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Laravel API Documentation",
+     *      description="L5 Swagger API Documentation for Laravel 10",
+     * )
+     */
 class UserController extends Controller
 {
+
+    /**
+     * @OA\Post(
+     *     path="/api/user/addUser",
+     *     tags={"Users"},
+     *     summary="Create a new user",
+     *     description="Returns user data",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="name", type="string", example="John Doe"),
+     *             @OA\Property(property="email", type="string", example="johndoe@example.com"),
+     *             @OA\Property(property="password", type="string", example="password123"),
+     *             @OA\Property(property="password_confirmation", type="string", example="password123"),
+     *             @OA\Property(property="role", type="string", example="admin")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User created"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad Request"
+     *     )
+     * )
+     */
     public function addUser(RegisterUserRequest $request)
     { 
         $validatedData = $request->validated();
@@ -21,7 +54,7 @@ class UserController extends Controller
             return response()->json(['error' => 'Role not found'], 404);
         }
   
-            // Create a new user
+        // Create a new user
         $user = User::create([
             'name' => $validatedData['name'],
             'email' => $validatedData['email'],
